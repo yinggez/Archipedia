e632461aa072d4380ffa9b639aeb2d12dfdaf881
Kemal Karakoç <42945832+KemalKrKX@users.noreply.github.com>
2025-08-01 17:08:38 +0200

    Update ProjectFilters.jsx
    Added PR year dropdown

--- src/components/ProjectFilters.jsx
+++ src/components/ProjectFilters.jsx
@@ -320,6 +320,30 @@ const ProjectFilters = ({ filters, setFilters }) => {
             <option value="SNT Visual">SNT Visual</option>
         </select>
       </label>
+      {/* PR Year */}
+      <label>
+        Year of First PR:
+        <select name="PR_year" onChange={handleChange}>
+            <option value="">All</option>
+            <option value="2003">2003</option>
+            <option value="2006">2006</option>
+            <option value="2009">2009</option>
+            <option value="2011">2011</option>
+            <option value="2012">2012</option>
+            <option value="2013">2013</option>
+            <option value="2015">2015</option>
+            <option value="2016">2016</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value=NULL>No Information</option>
+        </select>
+      </label>
         </div>
    
       );
