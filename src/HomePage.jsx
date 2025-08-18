39cddbfd98f38ea307213b9b713deb754853037e
yinggez <yinggezhoujess@gmail.com>
2025-08-18 21:13:35 +0200

    minor fix

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,7 +5,7 @@ export default function Homepage() {
     <div className="homepage">
       <h1>Welcome to ArchiPedia</h1>
       <h2>-presented to you by Kemal and Yingge</h2>
-      <p className="maintext">The ArchiPedia database currently consists of <strong>55</strong> urban future projects and <strong>704</strong>images. All information is gathered through a keyword search on <i>Nexis Uni</i>, from press releases published between <strong>2019 to 2024</strong>.* Images were mainly retrieved from the project websites, but if unavailable, a secondary website was used. Analysis of the media files was done using <i>MAXQDA</i>, where we grouped images with a list of "tags" that best describe them.</p>
+      <p className="maintext">The ArchiPedia database currently consists of <strong>55</strong> urban future projects and <strong>704</strong> images. All information is gathered through a keyword search on <i>Nexis Uni</i>, from press releases published between <strong>2019 to 2024</strong>.* Images were mainly retrieved from the project websites, but if unavailable, a secondary website was used. Analysis of the media files was done using <i>MAXQDA</i>, where we grouped images with a list of "tags" that best describe them.</p>
       <p className="maintext">Supervised and led by Dr. Nadia Alaily-Mattar, certain resources are provided by our university, TUM.<br></br>
       ArchiPedia serves as a pilot project for a future database of a similar nature, on a larger scale. </p>
       <Link to="/query" className="btn">
